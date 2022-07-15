c29052baf5efa39a7e10c9b9df196c424478bb73
BhaskarKulshrestha <95305804+BhaskarKulshrestha@users.noreply.github.com>
2022-07-15 15:04:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// console.log("Hello World")
+//     // alert("hi")
+// console.log("Hello ", 10*10 ,"My name is Bhaskar")
+
+//  declaration of the variables
+
+// var num1=5
+// var str1="Hi this is Bhaskar"
+// var obj={
+//     ravi:34,
+//     bhaskar:89,
+//     punit:65
+// }
+// console.log(obj)
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+
+// function table(a,b){
+//     for(i=1;i<=10;i++)
+//     {
+//         console.log("2 X ",i,"=",a*i)
+//     }
+// }
+// console.log("Print the table of 2 * 10 =")
+// console.log(table(2,10))
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+// arr.forEach(function(element){
+//    console.log(element)
+// })
+// a=10
+// let elem = document.getElementById('click')
+let elemClass = document.getElementsByClassName("container")
+// console.log(elemClass)
+// elemClass[0].style.background="yellow";
+// elemClass[0].classList.add("bg-primary")
+// elemClass[0].classList.add("bg-green")
+elemClass[0].innerHTML
